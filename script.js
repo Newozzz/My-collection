521d89f9f242b5121fd942d78fea6e6fc1a4f311
Newoz <josh.zaninello@gmail.com>
2023-12-14 16:37:12 +0100

    TOP 10

---
+++ script.js
@@ -0,0 +1,100 @@
+const collection = [
+    {
+        img: "assets/ronii.jpg",
+        name: "Nom: Ronaldinho",
+        age: "Age: 43 Ans",
+        pays: "Pays: Brésil",
+        club: "Club: Fc retraite",
+        position: "Poste: MOC",
+        stat: "Stat: Ballon d'or 2005"
+
+    },
+    {
+        img: "assets/maldini.jpg",
+        name: "Nom: Paolo Maldini",
+        age: "Age: 55 Ans",
+        pays: "Pays: Italie",
+        club: "Club: Fc retraite",
+        position: "Poste: DC",
+        stat: "Stat: 902 matchs avec l'AC Milan",
+
+    },
+    {
+        img: "assets/kaka.jpg",
+        name: "Nom: Kakà",
+        age: "Age: 41 Ans",
+        pays: "Pays: Brésil",
+        club: "Club: Fc retraite",
+        position: "Poste: MOC",
+        stat: "Stat: Ballon d'or 2007",
+    },
+    {
+        img: "assets/zlatan.jpg",
+        name: "Nom: Zlatan Ibrahimović",
+        age: "Age: 42 Ans",
+        pays: "Pays: Suède",
+        club: "Club: Fc retraite",
+        position: "Poste: BU",
+        stat: "Stat: A marqué dans toutes les minutes d'un match au cours de sa carrière",
+
+    },
+    {
+        img: "assets/neymar.jpg",
+        name: "Nom: Neymar jr",
+        age: "Age: 31 Ans",
+        pays: "Pays: Brésil",
+        club: "Club: Al-Hilal",
+        position: "Poste: AG",
+        stat: "Stat: 10 ème au ballon d'or en 2011 alors qu'il jouait au brésil",
+
+    },
+    {
+        img: "assets/Ruud_Gullit_-_Milan.jpg",
+        name: "Nom: Ruud Gullit",
+        age: "Age: 61 Ans",
+        pays: "Pays: Pays bas",
+        club: "Club: Fc retraite",
+        position: "Poste: MC",
+        stat: "Stat: Ballon d'or 1987",
+
+    },
+    {
+        img: "assets/hazardd.jpg",
+        name: "Nom: Eden Hazard",
+        age: "Age: 32 Ans",
+        pays: "Pays: Belgique",
+        club: "Club: Fc retraite",
+        position: "Poste: AG",
+        stat: "Stat: 3 fois dans l'équipe type de la saison de Premier League",
+
+    },
+    {
+        img: "assets/leao.jpg",
+        name: "Nom:  Rafael Leão ",
+        age: "Age: 24 Ans",
+        pays: "Pays: Portugal",
+        club: "Club: Ac milan",
+        position: "Poste: AG",
+        stat: "Stat: Meilleur joueur de l'année Serie A 2022",
+
+    },
+    {
+        img: "assets/cr7.jpg",
+        name: "Nom: Cristiano Ronaldo",
+        age: "Age: 38 Ans",
+        pays: "Pays: Portugal",
+        club: "Club: Al Naasr",
+        position: "Poste: BU",
+        stat: "Stat: 5 ballon d'or"
+    },
+    {
+        img: "assets/silva.jpg",
+        name: "Nom: Thiago Silva",
+        age: "Age: 39 Ans",
+        pays: "Pays: Brésil",
+        club: "Chelsea FC",
+        position: "Poste: DC",
+        stat: "Stat: 293 Matchs en tant que capitaine du PSG",
+    }
+]
+
